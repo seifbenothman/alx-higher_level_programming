3979bdc6984bac2050775d566e36c257c2ef911a
seifbenothman <seifbenothman95@gmail.com>
2023-10-03 23:45:10 +0000

    0

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for x in range(0, 10):
+    for r in range(1, 10):
+        if x >= r:
+            continue
+        elif x == 8 and r == 9:
+            print("{}{}".format(x, r))
+        else:
+            print("{}{}, ".format(x, r), end="")
