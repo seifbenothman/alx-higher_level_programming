119e9623a81dd18ee7023d832e64a1cd67ad8a93
seifbenothman <seifbenothman95@gmail.com>
2023-10-05 23:34:25 +0000

    0

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if num_arguments == 0:
         print("0 arguments.")
     elif num_arguments == 1:
-        print("1 arguments:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(num_arguments))
     for i in range(1, num_arguments + 1):
