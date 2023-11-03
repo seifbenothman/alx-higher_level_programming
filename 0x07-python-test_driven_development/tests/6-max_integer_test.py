86bf735112cc71b9221cbaff2456ef55db3aa2dd
seifbenothman <seifbenothman95@gmail.com>
2023-11-03 09:04:01 +0000

    0

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,38 +1,33 @@
 #!/usr/bin/python3
+"""Unittest for max_integer([..])
 """
-Unittest
-"""
-
 import unittest
-from max_integer import max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Test max int empty list"""
-    def test_max_integer_empty_list(self):
-        result = max_integer([])
-        self.assertIsNone(result)
+    """ tests for max_integer
+    """
 
-    def test_max_integer_single_element_list(self):
-        """Test single elem"""
-        result = max_integer([42])
-        self.assertEqual(result, 42)
+    def test_max_int_basic(self):
+        """ tests normal list of ints
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
-    def test_max_integer_positive_values(self):
-        """Test positive V"""
-        result = max_integer([1, 2, 3, 4])
-        self.assertEqual(result, 4)
+    def test_max_int_empty(self):
+        """ tests if list is empty
+        """
+        self.assertEqual(max_integer([]), None)
 
-    def test_max_integer_mixed_values(self):
-        """Test mixed V"""
-        result = max_integer([1, 3, 4, 2])
-        self.assertEqual(result, 4)
+    def test_max_int_neg(self):
+        """ tests if list has a negative int
+        """
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
-    def test_max_integer_negative_values(self):
-        """Test Negative V"""
-        result = max_integer([-1, -3, -4, -2])
-        self.assertEqual(result, -1)
-
+    def test_max_int_one(self):
+        """ tests if list has only one item
+        """
+        self.assertEqual(max_integer([1]), 1)
 
 if __name__ == '__main__':
     unittest.main()
