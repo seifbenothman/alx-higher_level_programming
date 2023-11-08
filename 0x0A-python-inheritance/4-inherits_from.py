f777355e37f352a7834f76dd1477e7d5562be715
seifbenothman <seifbenothman95@gmail.com>
2023-11-08 09:18:30 +0000

    0

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-Returns true if obj is instance of class that it inherits from or is subcls of
+class
 """
 
 
-def inherits_form(obj, a_class):
+def inherits_from(obj, a_class):
     """
-    Return:
-    true if obj is instance of class that it inherits from or is subcts of
+    determine a class
     """
-
     return issubclass(type(obj), a_class) and type(obj) is not a_class
