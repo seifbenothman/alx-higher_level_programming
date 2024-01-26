1ba2ec456024ef3c6b5b5db38349cf7b42d781bf
seifbenothman <seifbenothman95@gmail.com>
2024-01-26 15:15:40 +0000

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" REQUESTS - GitHub API """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_repo = f"{sys.argv[2]}/{sys.argv[1]}"
+    url = f"https://api.github.com/repos/{user_repo}/commits"
+
+    req = requests.get(url)
+    data = req.json()
+
+    for com in data:
+        name = com['commit']['author']['name']
+        sha = com['sha']
+        print(f"{sha}: {name}")
